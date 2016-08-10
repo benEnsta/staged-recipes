5b28390f8afdad89fb3484afcc3a11a63b09c902
Benoit DESROCHERS <ben.ensta@gmail.com>
2016-08-10 19:15:01 +0200

    [master] force release

--- recipes/ibexlib/bld.bat
+++ recipes/ibexlib/bld.bat
@@ -4,7 +4,7 @@ echo on
 echo %CMAKE_GENERATOR%
 cmake -G "%CMAKE_GENERATOR%" -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX="%PREFIX%" ../
 set MSBuildLogger="C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-set MSBuildOptions=/v:m /logger:%MSBuildLogger%
+set MSBuildOptions=/v:m /logger:%MSBuildLogger% /p:Configuration=Release
 echo %MSBuildOptions%
 msbuild %MSBuildOptions% INSTALL.vcxproj
 if errorlevel 1 exit 1
